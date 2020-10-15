256a1dad3231395a81a23ede52e5450458f578c7
lanapalmer <lanapalmer@lanas-mac-mini.attlocal.net>
2020-10-15 13:57:10 -0700

    Update front end to pass variable

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -13,7 +14,34 @@ def index():
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
-    return render_template('prediction.html')
+	df = process_inputs()
+
+	comment = df.Comment[0]
+	return render_template('prediction.html', variable='A BOT')
+   
+
+def process_inputs():
+	text_keys = ['Comment', 'Subreddit']
+	int_keys = ['Score', 'FlairCount']
+	inputs = {}
+
+	for key, val in request.form.items():
+		if key in int_keys:
+			inputs[key] = int(val)
+		else:
+			inputs[key] = val
+
+	return pd.DataFrame(inputs, index=[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+def process_inputs():
+    '''
+    Process input data for the model training.
+    '''
+
+    int_keys = ('Pclass', 'SibSp', 'Parch')
+    inputs = {}
